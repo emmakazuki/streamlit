9825a65f8ba17f5e98372cfc9c5a01b89608c302
emmakazuki <124504299+emmakazuki@users.noreply.github.com>
2023-02-05 22:52:21 +0900

    Delete beautifulsoup.py

--- beautifulsoup.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-res=requests.get("https://www.orangepage.net/")
-soup = BeautifulSoup(res.text,"html.parser")
-tag_obj=soup.find_all("p")
-text_list=[x.string for x in tag_obj]
-print(text_list[2])
